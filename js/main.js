d42905fed3242c5ff0f077ec4afaa605fe7ca5dc
Pritimanna <pritimanna102@gmail.com>
2021-12-13 19:14:02 -0800

    slider added

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('#menubtn').bind('click',function(){
         $(".menu ul").slideToggle(1000);
     });
+
+    $(".img-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        dots: true
+    });
 });
